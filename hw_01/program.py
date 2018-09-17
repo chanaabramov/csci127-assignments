319c9943dafcec55a7a0439d9417a2009a748212
Chana Abramov <chanaabramov@gmail.com>
2018-09-16 22:51:09 -0400

    hw is done

--- hw_01/program.py
+++ hw_01/program.py
@@ -2,10 +2,22 @@ def capitalize(name):
     space_index = name.find(" ")
     first = name[0:space_index]
     last = name[space_index+1:]
-    return capitalize(first[0]) + capitalize(last[0])
-
+    return first.capitalize() + " " + last.capitalize()
 print (capitalize("chana abramov"))
 
+def init(name) :
+    space_index = name.find(" ")
+    first = name[0:space_index]
+    initial = first[0]
+    last = name[space_index+1:]
+    return initial.capitalize() + ". " + last.capitalize()
+print(init("chana abramov"))
+
+def part_pig_latin(name):
+    middle_letters = name[1:]
+    first = name[0]
+    return middle_letters + first + "ay"
+print(part_pig_latin("chana"))
 
 def make_out_word(out, word):
   return out[0:2] + word  + out[2:4]
