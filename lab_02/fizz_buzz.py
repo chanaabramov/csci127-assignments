abee337ee0a9aa167d7e342d9dcc00f56c2fbeb3
Chana Abramov <chanaabramov@gmail.com>
2018-09-23 17:32:32 -0400

    fizzbuzz

--- lab_02/fizz_buzz.py
+++ lab_02/fizz_buzz.py
@@ -1,6 +1,6 @@
-x=1
-def fizz_buzz(x):
-    while x<=100 :
+def fizz_buzz(number):
+    x=1
+    while x<=number :
         if x % 3 == 0 and x % 5 == 0 :
             print("fizz" + "buzz")
         elif x % 3 == 0:
@@ -11,6 +11,6 @@ def fizz_buzz(x):
             print(x)
         x = x+1
         
-print(fizz_buzz(x))
+print(fizz_buzz(100))
             
         
