d0fa9c7c86ef873ee854f53256c4528279f0d562
Chana Abramov <chanaabramov@gmail.com>
2018-12-02 21:37:09 -0500

    C++ hw

--- exam_02/rle.py
+++ exam_02/rle.py
@@ -1,13 +1,27 @@
-d = {}
-def encode(s):
-    for i in range(1,len(s)):
-        d[s[0]] = 1
-        if s[i] == s[i-1]:
-            d[s[i]] = d[s[i]]+1
-        else:
-            d[s[i]] = 1
-    return(d)
-print(encode('abbaaccddee'))
+##d = {}
+##def encode(s):
+##    for i in range(1,len(s)):
+##        d[s[0]] = 1
+##        if s[i] == s[i-1]:
+##            d[s[i]] = d[s[i]]+1
+##        else:
+##            d[s[i]] = 1
+##    return(d)
+##print(encode('abbaaccddee'))
 
 ##def decode(list):
-    
+
+def encode(s):
+    for i in s:
+        l = []
+        l.append(i)
+        print(l)
+        count= 0
+        for count in range(1,len(s)):
+            if s[count] == s[count-1]:
+                count = count +1
+                l.append(count)
+                print(l)
+                
+            
+print(encode('abbaaccddee'))
---
+++ hw_09/count.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+  int number = 1;
+  while (number < 20)
+    {
+      std::cout << number <<" \n";
+      number++;
+    }
+  return 0;
+
+
+
+}
