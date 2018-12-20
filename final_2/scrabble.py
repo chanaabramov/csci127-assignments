3685a66134598e9c8d7f57bdab58960682a7bd57
Chana Abramov <chanaabramov@gmail.com>
2018-12-20 10:49:14 -0500

    Done

--- final_2/scrabble.py
+++ final_2/scrabble.py
@@ -12,6 +12,30 @@ def canMakeWord(letters,word):
         return True
     else:
         return False
+    
+def withWild(letters,word):
+    count = 0
+    for i in letters:
+        if i == "?":
+            count = count+ 1
+    yes = 0
+    for i in word:
+        if i in letters:
+            yes +=1
+        if i not in letters and count> 0 :
+            yes += 1
+            count = count- 1
+    if yes ==  len(word):
+        return True
+    else:
+        return False
+    
+            
+        
+    
 print(canMakeWord("dnsrtuscti" , "rustic"))
 print(canMakeWord("lkdmskjelkwfeqwjs" , "chana"))
+print(withWild("lkdcak?el?feqwhs" , "chana"))
+print(withWild("lkd??????????qwhs" , "chanachana"))
+print(withWild("lkdcak?elfeqwhs" , "change"))
     
